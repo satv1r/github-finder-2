bc34f90f23abc10b785bff80ea23c56a3120686e
Satvir Sandhu <356Sandhu@gmail.com>
2020-08-24 18:37:58 -0400

    add useritem

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
+import UserItem from "./components/users/UserItem";
 import "./App.css";
 
 class App extends Component {
@@ -7,7 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <Navbar />
-        <h1>Hello</h1>
+        <UserItem />
       </div>
     );
   }
